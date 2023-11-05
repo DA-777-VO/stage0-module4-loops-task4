b753d2103c3a4ec80ab10d8d16d1fed5208cea9b
David Apakin <apakin.david@gmail.com>
2023-11-05 20:32:32 +0400

    4.2.4

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -3,5 +3,15 @@ package school.mjc.stage0.loops.task4;
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
 
+        int i = 1;
+        do{
+            if (i % 3 == 0){
+                i++;
+                continue;
+            }
+            System.out.println(i);
+            i++;
+        } while (i <= lastPrinted);
+
     }
 }
